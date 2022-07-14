2b053026c3622de2f6d716544e27202784696d11
coofzilla <75731066+coofzilla@users.noreply.github.com>
2022-07-14 11:15:54 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'index.css';
 import App from 'components/App/App';
 
+//upgrade this to react 18 using createRoot
+//https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -10,6 +13,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
